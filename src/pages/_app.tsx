a9bc589c3ab3f6d864948379fc484ee49896a687
youjin <yjinhann@gmail.com>
2023-06-02 16:26:26 +0900

    feature: font 셋팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Noto_Sans_KR } from '@next/font/google';
+
+const notosanskr = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={notosanskr.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
