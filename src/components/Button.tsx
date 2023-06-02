722e9643c846a822a9b41f7be0d4fa8a4fd3d582
youjin <yjinhann@gmail.com>
2023-06-02 22:53:24 +0900

    feature: 버튼 컴포넌트 타입추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,13 @@
-import { Black_And_White_Picture } from '@next/font/google';
 import React from 'react';
 
-const Button = ({ children, onClick, disabled, buttonSize }) => {
+interface OwnProps {
+  children: string;
+  onClick(): void;
+  disabled: boolean;
+  buttonSize: 'small' | 'medium' | 'large';
+}
+
+const Button: React.FC<OwnProps> = ({ children, onClick, disabled, buttonSize }) => {
   const buttonActive = disabled
     ? 'bg-[#87712D] cursor-not-allowed text-black'
     : 'bg-[#F0C23A] text-black';
@@ -9,19 +15,19 @@ const Button = ({ children, onClick, disabled, buttonSize }) => {
   let buttonSizeClass = '';
   switch (buttonSize) {
     case 'small':
-      buttonSizeClass = 'py-2 px-5 text-sm rounded-2xl ';
+      buttonSizeClass = 'py-2 px-5 text-sm rounded-2xl';
       break;
     case 'large':
       buttonSizeClass = 'w-5/6 py-3.5 px-8 text-lg rounded-full font-medium mx-auto my-0 block';
       break;
     default:
-      buttonSizeClass = 'w-1/2 py-3.5  rounded-full mx-auto my-0 block ';
+      buttonSizeClass = 'w-1/2 py-3.5  rounded-full mx-auto my-0 block';
       break;
   }
 
   return (
     <button
-      className={`${buttonActive} ${buttonSizeClass} `}
+      className={`${buttonActive} ${buttonSizeClass}`}
       onClick={onClick}
       disabled={disabled}
       style={{ color: 'black' }}
