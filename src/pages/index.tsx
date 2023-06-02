7da92fbfe466e90771767af6e6741c85609315a7
youjin <yjinhann@gmail.com>
2023-06-02 16:51:33 +0900

    feature: tailwind-css 설치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <div>home</div>
+        <div className='text-4xl'>home</div>
       </main>
     </>
   );
