cefa400120cefec40e0eec9201d3e0063d756dd6
youjin <yjinhann@gmail.com>
2023-06-30 14:38:54 +0900

    fix: signupForm UI 수정

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -8,12 +8,7 @@ interface LabelProps {
 
 const Label: React.FC<LabelProps> = ({ htmlFor, children, labelText }) => {
   return (
-    <label
-      htmlFor={htmlFor}
-      className={`block text-xs ${htmlFor === 'email' ? '' : 'pb-4'} ${
-        htmlFor === 'email-domain' && 'px-3 pb-0 pt-5'
-      }`}
-    >
+    <label htmlFor={htmlFor} className='block text-xs pb-2 px-3 pt-5'>
       {labelText}
       {children}
     </label>
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -5,26 +5,27 @@ import Label from './Label';
 const signupForm = () => {
   return (
     <form className=' flex flex-col'>
-      <h1 className='text-2xl text-center py-10'>회원가입</h1>
+      <h1 className='text-2xl text-center py-3'>회원가입</h1>
 
       <Label htmlFor='userId' labelText='아이디'>
         <Input type='text' id='userId' placeholder='아이디를 입력하세요' />
       </Label>
 
       <Label htmlFor='password' labelText='비밀번호'>
-        <Input type='password' id='password' placeholder='비밀번호를 입력하세요' />
+        <Input
+          type='password'
+          id='password'
+          placeholder='비밀번호를 입력하세요'
+        />
       </Label>
 
-      <Label htmlFor='username' labelText='이름'>
-        <Input type='text' id='username' placeholder='이름을 입력하세요' />
+      <Label htmlFor='name' labelText='이름'>
+        <Input type='text' id='name' placeholder='이름을 입력하세요' />
       </Label>
 
-      <Label htmlFor='email' labelText='이메일' />
-      <div className='flex items-center pb-4'>
+      <Label htmlFor='email' labelText='이메일'>
         <Input type='text' id='email' placeholder='이메일을 입력하세요' />
-        <Label htmlFor='email-domain' labelText='@' />
-        <Input type='text' id='email-domain' />
-      </div>
+      </Label>
 
       <Label htmlFor='phone' labelText='전화번호'>
         <Input type='number' id='phone' placeholder='전화번호를 입력하세요' />
