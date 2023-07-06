f4066d2a86894fc9b3d57194cb7fe1fd51e66c2f
youjin <yjinhann@gmail.com>
2023-07-06 17:24:26 +0900

    feature: 카카오 토큰 받아오는 로직 추가

--- src/pages/kakaologin.tsx
+++ src/pages/kakaologin.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react';
+import { useMutation } from 'react-query';
+import { postKakaoLogin } from '../api/kakao-login';
 
 const KakaoLogin = () => {
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get('code');
     console.log(code);
   }, []);
+
+  const mutation = useMutation(postKakaoLogin, {
+    onSuccess(data) {},
+  });
+
   return <div>KakaoLogin</div>;
 };
 export default KakaoLogin;
