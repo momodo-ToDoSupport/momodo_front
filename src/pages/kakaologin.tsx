2d11d6c369a092bbd5e876041ae686614b388835
youjin <yjinhann@gmail.com>
2023-07-09 11:41:09 +0900

    fix: 함수호출로직수정

--- src/pages/kakaologin.tsx
+++ src/pages/kakaologin.tsx
@@ -1,16 +1,28 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMutation } from 'react-query';
-import { postKakaoLogin } from '../api/kakao-login';
+import { postKakaoLogin, sendKakaoToken } from '../api/kakao-login';
 
 const KakaoLogin = () => {
   const router = useRouter();
   const { code } = router.query;
   console.log(code);
 
-  // const mutation = useMutation(postKakaoLogin, {
-  //   onSuccess(data) {},
-  // });
+  useEffect(() => {
+    if (code) {
+      postKakaoLogin(code)
+        .then((data) => {
+          console.log(data);
+          console.log(data.access_token);
+          const accesstoken = data.access_token;
+          sendKakaoToken(accesstoken);
+        })
+        .catch((error) => {
+          console.error(error);
+          // 에러 처리 로직 추가
+        });
+    }
+  }, [code]);
 
   return <div>KakaoLogin</div>;
 };
