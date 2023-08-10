44dfb85b5359d5e205041efc6e0028813cd48a5b
Yangjisung <ygji6406@gmail.com>
2023-08-10 20:21:24 +0900

    rafact: layout.tsx 코드구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,12 @@ import { Noto_Sans_KR } from "@next/font/google";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { CookiesProvider } from "react-cookie";
+import { kakaoAuthUrl } from "../api/kakao-login";
 import logo from "../../public/images/logo.svg";
 import letterlogo from "../../public/images/momodo.svg";
 import Image from "next/image";
 import LoginButton from "../components/LoginButton";
 import Link from "next/link";
-
-import { kakaoAuthUrl } from "../api/kakao-login";
 
 const notosanskr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -31,29 +30,11 @@ export default function RootLayout({
         <main className="flex flex-col items-center justify-end h-screen">
           <CookiesProvider>
             <QueryClientProvider client={queryClient}>
-              <h1 className="h-4/5">
-                <Image src={logo} className="mb-6" alt="로고 이미지" />
-                <Image src={letterlogo} alt="레터링 로고 이미지" />
-              </h1>
-              <div className="text-center absolute mb-20">
-                <LoginButton type="kakao" href={kakaoAuthUrl}>
-                  카카오톡 계정으로 로그인
-                </LoginButton>
-                <LoginButton type="google" href="/signup">
-                  구글 계정으로 로그인
-                </LoginButton>
-                <LoginButton type="momodo" href="/login">
-                  모모두 계정으로 로그인
-                </LoginButton>
-                <Link href="signup">
-                  <button className="block mx-auto my-0">회원가입</button>
-                </Link>
-              </div>
+              {children}
               <ReactQueryDevtools initialIsOpen={false} />
             </QueryClientProvider>
           </CookiesProvider>
         </main>
-        {children}
       </body>
     </html>
   );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,6 @@
-import { useRouter } from 'next/router';
+"use client";
+
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 import { postSignup } from '../../api/auth';
