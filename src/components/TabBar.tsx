8bf92959357d3c7af851b9aa3e71456c82fca787
youjin <yjinhann@gmail.com>
2023-08-14 23:41:31 +0900

    feature: 라우터path에 따른 탭메뉴 활성화

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,20 +1,17 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
-import homeIcon from '../../public/images/homeIcon.svg';
+import todoIcon from '../../public/images/todoIcon.svg';
 import searchIcon from '../../public/images/searchIcon.svg';
-import chatIcon from '../../public/images/chatIcon.svg';
+import feedIcon from '../../public/images/feedIcon.svg';
 import profileIcon from '../../public/images/profileIcon.svg';
 import TabBarIcon from './TabBarIcon';
 
 const TabBar = () => {
-  // TabBar activeIcon으로 변경하는 로직 추가하기
   return (
     <ul className='flex justify-around fixed bottom-0 left-0 right-0 pb-4 w-full bg-bg-color'>
-      <TabBarIcon src={homeIcon} name='피드' />
-      <TabBarIcon src={searchIcon} name='검색' />
-      <TabBarIcon src={chatIcon} name='채팅' />
-      <TabBarIcon src={profileIcon} name='프로필' />
+      <TabBarIcon src={todoIcon} name='투두' path='mytodo' />
+      <TabBarIcon src={searchIcon} name='검색' path='search' />
+      <TabBarIcon src={feedIcon} name='피드' path='feed' />
+      <TabBarIcon src={profileIcon} name='프로필' path='myprofile' />
     </ul>
   );
 };
