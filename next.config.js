641f66573213db8257b63cd9500675f8aa613eff
Yangjisung <ygji6406@gmail.com>
2023-09-03 22:45:15 +0900

    fix :  배포URL(https) 변경

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/authentication/token',
-        destination: `https://18.215.108.155:8080/api/v1/authentication/token`,
-      }
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/v1/authentication/token',
+  //       destination: `https://18.215.108.155:8080/api/v1/authentication/token`,
+  //     }
+  //   ]
+  // },
   experimental: {
     serverActions: true,
   },
