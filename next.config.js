5855cd496171be8f14036ae54a99e5643ce2cef4
Yangjisung <ygji6406@gmail.com>
2023-09-03 21:49:11 +0900

    fix : URL 경로 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/authentication/token',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      }
+    ]
+  },
   experimental: {
     serverActions: true,
   },
